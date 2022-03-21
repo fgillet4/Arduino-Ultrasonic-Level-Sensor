ac275adbdde710f8e2b4c1b5ebda406bc771e64a
Francis Gillet <39193693+fgillet4@users.noreply.github.com>
2022-03-21 20:29:15 +0100

    Update LCD_COVID_SENSOR.ino

--- LCD_COVID_SENSOR.ino
+++ LCD_COVID_SENSOR.ino
@@ -1,7 +1,7 @@
 // Covid Sensor!
 // Author: Francis Gillet
-// Date 03/30/2021
-// Description: Stay 6 feet away from me! Ultrasonic sensor will measure distance and prinbt to the lcd if someone comes too close!
+// Date 03/21/2022
+// Description: Measure the flowrate over a weir using water level and height of wier
 
 #include <LCD16x2.h> // Library from Olimex LCD 16*2 screen
 #include <Wire.h>
@@ -11,18 +11,28 @@
 int trigPIN = 4; // Trigger US pulse
 int echoPIN = 5 ; // Echo received US pulse
 double duration = 0; // This is a function of speed of sound that will map the distance
-double speedOfSound = 0.001; // ft/micro second
-double distance_meters = 0;
+double speedOfSound_US = 0.001; // ft/micro second
+double speedOfSound_SI = 0.000343 // m/micro second
+double distance_meters = 0; // distance sensor is from the waters edge
 double distance_feet = 0;
-LCD16x2 lcd; //Declare the object
+double length_total = 6; // [meter](Measured out in field sensor to bottom)
+double water_height = 0; // changes as more or less water flows through
+const double weir_height = 2.9;// in meters
+double flowrate_cum = 0; //
+double flowrate_liters = 0;
+const double velocity = 0.1 //
+const double friction_constant = 0.5
+const double weir_width = 0.8 //meters
+double water_tower_height = 0; // storing the pressure
+LCD16x2 lcd; //Declare the lcd panel as an object to be able to interact with its library
 
-int buttons;
+int buttons; //not sure
 
 void setup(){
   Serial.begin(9600);
   Wire.begin();
   lcd.lcdClear();
-  pinMode(trigPIN, OUTPUT);
+  pinMode(trigPIN, OUTPUT); //Sets the trigPIN as the outputsignal
   pinMode(echoPIN, INPUT);
 }
 void loop(){
@@ -36,14 +46,20 @@ void loop(){
   duration = pulseIn(echoPIN, HIGH);
   distance_feet = (duration*speedOfSound)/2;
   distance_feet = round(distance_feet);
-  //distance_feet = distance_meters /0.3048;
+  distance_meters = distance_feet * 0.3048;
+  water_tower_height = length_total - weir_height - distance_meters;
+  flowrate_cum = (weir_width*pow((2*9.81*water_tower_height + pow(velocity,2)),(3/2)))/(3*9.81)- weir_width*pow(velocity,3)/(3*9.81)
+  flowrate_liters = flowrate_cum*1000
+  Serial.println(flowrate_liters);
+  
+  
   lcd.lcdGoToXY(2,1);
-  lcd.lcdWrite(distance_feet);
+  lcd.lcdWrite(distance_meters);
   lcd.lcdGoToXY(2,2);
-  lcd.lcdWrite("Feet");
+  lcd.lcdWrite("L/s");
   //Serial.println(duration);
   //Serial.println(distance_meters);
-  Serial.println(distance_feet);
+  Serial.println(flowrate_liters);
   delay(500);
 
 }
